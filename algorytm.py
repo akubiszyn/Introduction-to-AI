e8458638ae59d5249d60866dfde4abcd091e904f
Alicja Kubiszyn <alicja.kubiszyn.stud@pw.edu.pl>
2022-10-20 09:43:08 +0200

    fixed algorithm

--- algorytm.py
+++ algorytm.py
@@ -4,15 +4,22 @@ from autograd import grad
 list_of_xk = []
 
 def fun(x):
-    return x**2 + 5
+    for i in range(len(x)):
+        x[i] = x[i]**2 * 1**((i - 1)/ 9)
+    y = x.sum()
+    return y
 
 def get_xk(function, x0, a):
     gradient= grad(function)
-    xk = x0 - a * gradient(x0)
+    xk = np.zeros
+    for i in range(len(x)):
+        xk[i] = x0[i] - a * gradient(x0, i)
+    xk = x0 - a * gradient(x0, i)
     return xk
 
 def compute_proper_xk(function, x0, a):
-    xk = get_xk(function, x0, a)
+    #xk = get_xk(function, x0, a)
+    xk = np.arange(10)**3
     k = 5
     while function(xk) >= function(x0):
         k -= 1
@@ -22,13 +29,14 @@ def compute_proper_xk(function, x0, a):
             return
     return xk
 
-def gradient_algorithm(function, x0, a, k):
+def gradient_algorithm(function, x0, a):
+    print(x0.size)
     xk = compute_proper_xk(function, x0, a)
-    if abs(xk - x0) < 10**(-3):
+    if abs(function(xk) - function(x0)) < 10**(-3):
         return xk
     list_of_xk.append(xk)
     xk = gradient_algorithm(function, xk, a, k=5)
     return xk
 
-
-print(gradient_algorithm(fun, np.array([20]), 0.7, 5))
+y = fun(np.arange(10)**3)
+print(gradient_algorithm(fun, np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=float), 0.7))
--- plot.py
+++ plot.py
@@ -6,11 +6,11 @@ from algorytm import fun, list_of_xk
 def generate_plot():
     keys1 = [ i for i in range(-10, 11)]
     values1 = [i**2 + 5 for i in range(-10, 11)]
-    keys2 = list_of_xk
+    keys2 = [i for i in range(len(list_of_xk))]
     values2 = [fun(xk) for xk in list_of_xk]
     plt.plot(keys1, values1, '-')
     plt.plot(keys2, values2, 'o')
-    plt.legend()
+    #plt.legend()
     #plt.title()
     #plt.xticks(rotation=30, fontsize='xx-small', horizontalalignment='right')
     #buffer = BytesIO()
